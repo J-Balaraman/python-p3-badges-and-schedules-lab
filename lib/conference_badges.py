31de4d15088b3dc51bb67bc314d45031ac3487a4
jbalaraman <james.balaraman@gmail.com>
2023-11-25 18:08:55 -0700

    Explored for loop iteration

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    hello_message = []
+    for name in names:
+        hello_message.append(f"Hello, my name is {name}.")
+    return hello_message
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    room_num = 1
+    for name in names:
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {room_num}!")
+        room_num += 1
+    return room_assignments
 
 def printer(names):
-    return None
+    room_num = 1
+    for name in names:
+        print(f"Hello, my name is {name}.")
+    for name in names:
+        print(f"Hello, {name}! You'll be assigned to room {room_num}!")
+        room_num += 1
